105df5fc8e1af7096d41f91bb4031519b2b0532b
SEOINJAE <69412493+SEOINJAE@users.noreply.github.com>
2022-04-05 15:15:32 +0900

    Create db_ch3-4_assignment.sql
    savepoint

---
+++ 6주차/db_ch3-4_assignment.sql
@@ -0,0 +1,30 @@
+SHOW DATABASES;
+
+USE MYBOOKSTORE;
+
+
+/* 24) 가장 비싼 도서의 이름은 무엇인가? */
+SELECT bookname
+FROM book
+WHERE price IN (SELECT MAX(price)
+				FROM book)
+;
+
+SELECT MAX(price) -- 도서 이름 출력이 불가능
+FROM book
+;
+
+SELECT bookname
+FROM book
+ORDER BY price DESC
+LIMIT 1
+;
+
+/* 25) 도서를 구매한 적이 있는 고객의 이름을 검색하시오. */
+SELECT name
+FROM Customer as a
+WHERE custid IN (SELECT DISTINCT custid
+				 FROM Orders)
+;
+
+
