c70ab242831e92f60dffa93f9fba4c9e67c842d6
SEOINJAE <69412493+SEOINJAE@users.noreply.github.com>
2022-04-20 18:20:31 +0900

    7주차 Done

--- 7주차/db_ch4_assignment.sql
+++ 7주차/db_ch4_assignment.sql
@@ -6,20 +6,18 @@ SHOW TABLES;
 SELECT name
 	, saleprice
 FROM Customer as a
-	LEFT JOIN Orders as b on a.custid = b.custid
-;
+	LEFT JOIN Orders as b on a.custid = b.custid;
+    
 /* 2) 주소에 '대한민국'을 포함하는 고객들로 구성된 뷰를 만들고 조회하시오.
 	단, 뷰의 이름은 vw Customer로 한다. */
 
 CREATE VIEW vw_Customer as
 SELECT *
 FROM customer
-WHERE address LIKE '%대한민국%'
-;
+WHERE address LIKE '%대한민국%';
 
 SELECT *
-FROM vw_Customer
-;
+FROM vw_Customer;
 
 
 /* 3) Orders 테이블에 고객이름과 도서이름을 바로 확인할 수 있는 뷰를 생성한 후, 
@@ -30,8 +28,7 @@ SELECT a.*
     ,c.bookname, c.publisher, c.price
 FROM orders as a
 	INNER JOIN customer as b on a.custid = b.custid
-	INNER JOIN book as c on a.bookid = c.bookid
-;
+	INNER JOIN book as c on a.bookid = c.bookid;
 
 DESC vw_info_customer;
 
@@ -42,8 +39,8 @@ SELECT bookid
 	, bookname
     , saleprice
 FROM vw_info_customer
-WHERE name = '김연아'
-;
+WHERE name = '김연아';
+
 
 /* 4) 앞서 생성한 뷰 vw_Customer를 삭제하여라. */
 DROP VIEW vw_Customer;
@@ -60,7 +57,7 @@ FROM vw_info_customer;
 
 /* 실습: 서점 데이터(Cascading Actions) */
 # Orders 테이블에 수정/삭제에 대한 cascade 옵션을 추가하시오.
-set sql_safe_updates=0; # Error Code: 1175 일시적으로 세이프 모드 해제
+SET sql_safe_updates=0; # Error Code: 1175 일시적으로 세이프 모드 해제
 
 /* 제약조건 검색 쿼리 */
 SELECT * 
@@ -71,17 +68,16 @@ WHERE TABLE_NAME = 'orders';
 ALTER TABLE Orders 
 ADD CONSTRAINT orders_ibfk_customer
 FOREIGN KEY(custid) REFERENCES Customer (custid)
-ON DELETE CASCADE
-ON UPDATE CASCADE
-;
+					ON DELETE CASCADE
+					ON UPDATE CASCADE;
 
 /* orders 테이블 CASCADE 옵션 추가 */
 ALTER TABLE Orders 
 ADD CONSTRAINT orders_ibfk_book
 FOREIGN KEY(bookid) REFERENCES Book (bookid) 
-ON DELETE CASCADE
-ON UPDATE CASCADE
-;
+					ON DELETE CASCADE
+					ON UPDATE CASCADE;
+                    
 
 /* 제약조건 생성 결과 확인 */
 SELECT * 
@@ -95,22 +91,20 @@ ALTER TABLE Orders DROP FOREIGN KEY orders_ibfk_2;
 # Customer 테이블에서 ‘박지성’ 고객을 삭제
 DELETE
 FROM Customer 
-WHERE name = '박지성'
-;
+WHERE name = '박지성';
+
 
 # Customer 테이블에서 ‘김연아’ 고객의 custid를 12로 변경
 UPDATE Customer
 SET custid = '12'
-WHERE name = '김연아'
-;
+WHERE name = '김연아';
 
 SELECT * FROM customer;
 
 # Book 테이블에서 ‘굿스포츠’에서 출판한 모든 책을 삭제
 DELETE
 FROM book
-WHERE publisher = '굿스포츠'
-;
+WHERE publisher = '굿스포츠';
 
 SELECT * FROM book;
 
--- 7주차/db_ch4_practice.sql
+++ 7주차/db_ch4_practice.sql
@@ -4,9 +4,51 @@ USE mybookstore;
 
 SHOW TABLES;
 
+# CASCADE 옵션 실습
+/* 테이블 삭제 */
+-- DROP TABLE instructor;
+-- DROP TABLE department;
+
+/* Department 테이블 먼저 생성 (참조가 될 테이블) */
+CREATE TABLE department (
+    dept_name   	VARCHAR (20), 
+    building        VARCHAR (15), 
+    budget          NUMERIC (12,2),
+    PRIMARY KEY (dept_name)
+);
+
+/* Department 테이블의 dept_name 컬럼을 참조하는 
+instructor 테이블의 dept_name 컬럼 */
+CREATE TABLE instructor (
+    ID               CHAR(5),
+    name          	 VARCHAR(20) NOT NULL,
+    dept_name  		 VARCHAR(20),
+    alary            NUMERIC(8,2),
+    PRIMARY KEY (ID),
+    FOREIGN KEY (dept_name) REFERENCES 
+                 department (dept_name)
+                -- ON DELETE CASCADE
+                -- ON UPDATE CASCADE
+);
+
+/* 참조가될 department 테이블에 먼저 데이터 삽입 */
+INSERT INTO department VALUES ('biology', 'watson', '90000');
+INSERT INTO department VALUES ('comp. sci.', 'taylor', '100000');
+
+/* instructor 테이블에 데이터 삽입 */
+INSERT INTO instructor VALUES ('10101', 'srini', 'comp. sci.', 65000);
+INSERT INTO instructor VALUES ('45565', 'katz', 'comp. sci.', 75000);
+
+/* 데이터 확인 */
+SELECT * FROM instructor;
+SELECT * FROM department;
+
+/* Finance라는 부서명을 가지고 있는 Wu를 instructor 테이블에 입력 */
+INSERT INTO instructor VALUES ('12121', 'Wu', 'Finance', 90000);
+
+-- --------------------------------------------------------------------------
 # ON DELETE CASCADE 옵션 실습
 
-/* 테이블 삭제 */
 DROP TABLE instructor;
 DROP TABLE department;
 
@@ -40,22 +82,15 @@ INSERT INTO department VALUES ('comp. sci.', 'taylor', '100000');
 INSERT INTO instructor VALUES ('10101', 'srini', 'comp. sci.', 65000);
 INSERT INTO instructor VALUES ('45565', 'katz', 'comp. sci.', 75000);
 
-/* 데이터 확인 */
-SELECT * FROM instructor;
-
-SELECT * FROM department;
-
-
 /* ON DELETE CASCADE 옵션 때문에 참조관계와 상관없이 데이터 삭제 가능 */
 DELETE FROM department WHERE dept_name='comp. sci.';
 
 /* 데이터 변경 확인 */
 SELECT * FROM instructor;
-
 SELECT * FROM department;
 
 #----------------------------------------------------------------------------
-# ON DELETE UPDATE 실습
+# ON UPDATE CASCADE실습
 
 /* 이전에 생성된 테이블 삭제 후 다시 생성 */
 DROP TABLE instructor;
@@ -91,10 +126,8 @@ INSERT INTO department VALUES ('comp. sci.', 'taylor', '100000');
 INSERT INTO instructor VALUES ('10101', 'srini', 'comp. sci.', 65000);
 INSERT INTO instructor VALUES ('45565', 'katz', 'comp. sci.', 75000);
 
-
 /* 생성한 데이터 확인 */
 SELECT * FROM instructor;
-
 SELECT * FROM department;
 
 
@@ -105,7 +138,6 @@ UPDATE department SET dept_name='CS' WHERE dept_name='Comp. Sci.';
 
 /* 데이터 변경 확인 */
 SELECT * FROM instructor;
-
 SELECT * FROM department;
 
 
